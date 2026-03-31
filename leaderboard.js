62dbd7a4b2a4e78d226b055569ec0e822d610699
jamilayoub <jamil.ayoub.2007@gmail.com>
2026-04-01 00:33:30 +0300

    Add files via upload

---
+++ leaderboard.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){ 
+    // this line of code allows the page to load completely then run the JavaScript
+
+let table = document.querySelector(".leaderboard-table");
+
+console.log("Leaderboard Loaded");
+
+});
