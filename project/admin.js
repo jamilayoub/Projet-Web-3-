ed0a8b965f9985721402b8f913045bfc74a223c2
jamilayoub <jamil.ayoub.2007@gmail.com>
2026-06-13 10:46:13 +0300

    Delete project directory

--- project/admin.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-// again, this code will make Java script wait the page to load then js will run
-
-console.log("Admin dashboard loaded");
-
-});
